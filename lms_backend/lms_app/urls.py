6c8c534af5705bc2ad5504eec2af61165d9bec98
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2025-04-15 12:20:56 +0800

    chore: initialize frontend with react

--- lms_backend/lms_app/urls.py
+++ lms_backend/lms_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token 
 from . import views
 
 router = DefaultRouter()
@@ -10,4 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('courses/', views.CourseList.as_view()),
     path('course-categories/', views.CourseCategoryList.as_view()),
+    path('api-tokens-auth/', obtain_auth_token)
 ]
--- lms_backend/lms_backend/settings.py
+++ lms_backend/lms_backend/settings.py
@@ -107,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
