1d3ebc2c647c1cb44b582c2e7af1f857dba0ed44
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2025-05-04 20:38:52 +0800

    feat: thumbnail on courses, get avg ratings on courses

--- lms_backend/lms_app/models.py
+++ lms_backend/lms_app/models.py
@@ -10,13 +10,32 @@ class User(AbstractUser):
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(blank=True)
     description = models.TextField()
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses', limit_choices_to={'is_instructor': True})
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     language = models.CharField(max_length=100)
+    learn = models.JSONField(default=list, blank=True)
+    topics = models.JSONField(default=list, blank=True)
+    inclusion = models.JSONField(default=list, blank=True)
+    requirements = models.JSONField(default=list, blank=True)
+    preview_url = models.URLField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     is_published = models.BooleanField(default=False)
+
+    @property
+    def average_rating(self):
+        reviews = self.reviews.all()
+        total_reviews = reviews.count()
+        if total_reviews > 0:
+            total_rating = sum([review.rating for review in reviews])
+            return total_rating / total_reviews
+        return 0 
+    
+    @property
+    def rating_count(self):
+        return self.reviews.count()
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
